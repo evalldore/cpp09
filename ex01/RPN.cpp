dfad2bdd16c0eb3ad7ebab2d46e21be041650e44
evalldore <evalldore@gmail.com>
2024-03-05 16:26:30 -0500

    ex01 stuff

---
+++ ex01/RPN.cpp
@@ -0,0 +1,9 @@
+#include "RPN.hpp"
+
+int calc(const std::string str) {
+	std::string::const_iterator it;
+	std::stack<int> stack;
+	for (it = str.begin(); it != str.end(); it++) {
+
+	}
+}
---
+++ ex01/RPN.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+#include <stack>
+
+int calc(const std::string);
