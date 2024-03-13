9d0110b49bd3ae494400d1efa6199ed551de89c8
evalldore <evalldore@gmail.com>
2024-03-12 23:01:47 -0400

    vector init

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,29 @@
+#include <iostream>
+#include <cstdlib>
 #include "PmergeMe.hpp"
 
+static bool init(std::vector<int>& vec, int argc, char *argv[]) {
+	int		value;
+	char	*end;
+	for (int index = 1; index < argc; index++) {
+		value = static_cast<int>(strtol(argv[index], &end, 10));
+		if (*end)
+			return false;
+		vec.push_back(value);
+	}
+	return true;
+}
+
 int main(int argc, char* argv[]) {
-	(void)argc;
-	(void)argv;
+	if (argc < 3) {
+		std::cerr << "Not enough arguments in parameters!" << std::endl;
+		return 1;
+	}
+		
+	std::vector<int> intVec;
+	if (!init(intVec, argc, argv)) {
+		std::cerr << "Invalid parameters to initialize int vector!" << std::endl;
+		return 1;
+	}
 	return 0;
 }
