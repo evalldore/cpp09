4bdaa12b4a4d340b09a1a165f4656aba61fbe00e
Eliott Vallee dore <evalldore@gmail.com>
2024-04-10 17:16:30 -0400

    template function for ex02

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -4,6 +4,52 @@
 #include <limits>
 #include <utility>
 
-typedef std::vector<std::pair<int, int> > pair_vec_t;
+template <typename T>
+static void insertionSort(T& container, int start, int end) {
+	for (int i = start + 1; i <= end; ++i) {
+		int key = container[i];
+		int j = i - 1;
+		
+		while (j >= start && container[j] > key) {
+			container[j + 1] = container[j];
+			--j;
+		}
+		container[j + 1] = key;
+	}
+}
 
-void mergeInsertionSort(std::vector<int>&);
+template <typename T>
+static void merge(T& container, int start, int mid, int end) {
+	T temp(end - start + 1);
+	int i = start, j = mid + 1, k = 0;
+
+	while (i <= mid && j <= end) {
+		if (container[i] < container[j])
+			temp[k++] = container[i++];
+		else
+			temp[k++] = container[j++];
+	}
+
+	while (i <= mid)
+		temp[k++] = container[i++];
+
+	while (j <= end)
+		temp[k++] = container[j++];
+
+	for (int i = 0; i < k; ++i)
+		container[start + i] = temp[i];
+}
+
+template <typename T>
+void fordJohnsonMergeInsertionSort(T& container, int start, int end) {
+	if (start < end) {
+		int mid = (start + end) / 2;
+		if (end - start + 1 <= 10) {
+			insertionSort(container, start, end);
+		} else {
+			fordJohnsonMergeInsertionSort(container, start, mid);
+			fordJohnsonMergeInsertionSort(container, mid + 1, end);
+			merge(container, start, mid, end);
+		}
+	}
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,11 +7,20 @@ static bool init(std::vector<int>& vec, int argc, char *argv[]) {
 	char	*end;
 	for (int index = 1; index < argc; index++) {
 		value = static_cast<int>(strtol(argv[index], &end, 10));
-		if (*end)
+		if (*end || value < 0)
 			return false;
 		vec.push_back(value);
 	}
 	return true;
+}
+
+static void printVec(const std::vector<int>& vec) {
+	std::vector<int>::const_iterator it;
+
+	for (it = vec.begin(); it != vec.end(); it++) {
+		std::cout << *it << ' ';
+	}
+	std::cout << std::endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -22,9 +31,13 @@ int main(int argc, char* argv[]) {
 		
 	std::vector<int> intVec;
 	if (!init(intVec, argc, argv)) {
-		std::cerr << "Invalid parameters to initialize int vector!" << std::endl;
+		std::cerr << "Error: Invalid parameters" << std::endl;
 		return 1;
 	}
-	mergeInsertionSort(intVec);
+	std::cout << "Before: ";
+	printVec(intVec);
+	fordJohnsonMergeInsertionSort(intVec, 0, intVec.size() - 1);
+	std::cout << "After: ";
+	printVec(intVec);
 	return 0;
 }
